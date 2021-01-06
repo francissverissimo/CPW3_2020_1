95e61eaa42825b4e46568eabd18070b68a10203a
francissverissimo <francissv97@gmail.com>
2021-01-06 18:47:21 -0400

    Add latest project

---
+++ autenticacaoFiltroDeAcessoJWT/src/models/Usuario.js
@@ -0,0 +1,16 @@
+export default class Usuario {
+
+    /**
+     * Construtor da classe
+     * @param {*} login Login do usuário
+     * @param {*} nome  Nome do usuário
+     * @param {*} salt  String hexadecimal utilizada para "embaralhar" a senha do usuário
+     * @param {*} hash  Código criptografado gerado com base na senha do usuário
+     */
+    constructor(login, nome, salt, hash) {
+        this.login = login;
+        this.nome = nome;
+        this.salt = salt;
+        this.hash = hash;
+    }
+}
